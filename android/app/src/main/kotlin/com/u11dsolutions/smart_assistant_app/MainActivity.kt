b7b4bf1c175d0f16d63951ad99739cfd568531b8
anjanchaudhuri <anjan.chaudhuri@11dsolutions.com>
2021-04-28 15:02:58 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/u11dsolutions/smart_assistant_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.u11dsolutions.smart_assistant_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,462 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// import 'package:flutter/services.dart';
+import 'package:dialogflow_v2/dialogflow_v2.dart' as df;
+
+// import 'my_dialogflow_v2.dart' as df;
+// import 'my_auth_google.dart';
+// import 'my_language.dart';
+// import 'my_message.dart';
+import 'package:intl/intl.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'dart:async';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Smart Assistant',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.green,
+      ),
+      home: MyHomePage(title: 'Aramco Digital Assistant'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  final TextEditingController _textController = TextEditingController();
+  bool _micOn = false;
+  Future<void> _launched;
+
+  Future<void> launchInBrowser(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw ('Could not launch $url');
+    }
+  }
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: handleSubmitted,
+                decoration:
+                    InputDecoration.collapsed(hintText: 'Send a message'),
+              ),
+            ),
+            Container(
+              // margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: Icon(Icons.send),
+                onPressed: () => handleSubmitted(_textController.text),
+              ),
+            ),
+            Container(
+              // margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: Icon(_selectMicIcon()),
+                onPressed: () => _toggleMicState(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  IconData _selectMicIcon() {
+    return _micOn ? Icons.mic_rounded : Icons.mic_off_rounded;
+  }
+
+  void _toggleMicState() {
+    _micOn = (_micOn ? false : true);
+    setState(() {
+      _selectMicIcon();
+    });
+    print('Mic is ${_micOn ? 'on' : 'off'}');
+  }
+
+  void response(query) async {
+    _textController.clear();
+    df.AuthGoogle authGoogle =
+        await df.AuthGoogle(fileJson: '.secret/smart_assistant.json').build();
+    df.Dialogflow dialogflow = df.Dialogflow(
+        authGoogle: authGoogle,
+        language: df.Language.english //DateTime.now().toIso8601String()
+        );
+    df.DetectIntentResponse response = await dialogflow.detectIntent(query);
+
+    print("---- debug info $query -----");
+    print(
+        'Number of messages in response: ${response.getListMessage().length}');
+    for (int i = 0; i < response.getListMessage().length; ++i) {
+      String msgType = response.getListMessage()[i] == null
+          ? null
+          : response.getListMessage()[i].runtimeType.toString();
+      print('$i: $msgType');
+    }
+    // final DateFormat format = DateFormat('E MMM d, yyyy hh:mm');
+    // print(format.format(DateTime.now()));
+    // print(response.toJson());
+    print("---- end debug info -----");
+
+    // Display the user query text after obtaining their query text
+    // from STT api call, when the user was speaking to the bot.
+    if (response.outputAudioConfig != null) {
+      // Output is an audio in response to input audio
+      // So, the user input text is obtained from speech-to-text api call
+      // No explicit text was typed by the user
+      ChatMessage userQuery = ChatMessage(
+        text: response.queryResult.queryText,
+        name: 'User',
+        type: true,
+        now: DateTime.now(),
+      );
+      setState(() {
+        _messages.insert(0, userQuery);
+      });
+    }
+
+    // Display bot response (rich)
+    ChatMessage message = ChatMessage(
+      text: response.getMessage(),
+      aiResponse: response,
+      name: 'Bot',
+      type: false,
+      now: DateTime.now(),
+      pageState: this,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  // Submit user query from keyboard input (text message)
+  void handleSubmitted(String text) {
+    _textController.clear();
+    ChatMessage message = ChatMessage(
+      text: text,
+      name: 'User',
+      type: true,
+      now: DateTime.now(),
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+
+    // Build DetectIntentRequest
+    df.DetectIntentRequest request = df.DetectIntentRequest(
+        queryInput: df.QueryInput(
+            text: df.TextInput(text: text, languageCode: df.Language.english)));
+
+    // Call dialogflow
+    response(request);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: <Widget>[
+          Flexible(
+              child: ListView.builder(
+            padding: EdgeInsets.all(8.0),
+            reverse: true,
+            itemBuilder: (_, int index) => _messages[index],
+            itemCount: _messages.length,
+          )),
+          Divider(height: 1.0),
+          Container(
+            decoration: BoxDecoration(color: Theme.of(context).cardColor),
+            child: _buildTextComposer(),
+          ),
+        ],
+      ),
+    );
+    // throw UnimplementedError();
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  final String text;
+  final df.DetectIntentResponse aiResponse;
+  final String name;
+  final bool type;
+  final DateTime now;
+  final _MyHomePageState pageState;
+
+  // Constructor
+  ChatMessage(
+      {this.text,
+      this.aiResponse,
+      this.name,
+      this.type,
+      this.now,
+      this.pageState});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      margin: EdgeInsets.symmetric(
+        vertical: 10.0,
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: this.type ? myMessage(context) : otherMessage(context),
+      ),
+    );
+    // throw UnimplementedError();
+  }
+
+  String _format(DateTime moment) {
+    String _formattedDate;
+    DateFormat _formatter;
+    DateTime today =
+        DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
+    DateTime _eventDay = DateTime(moment.year, moment.month, moment.day);
+    int _dateDifference = today.difference(_eventDay).inDays;
+
+    print('Date Difference: $_dateDifference');
+
+    if (_dateDifference == 0) {
+      _formatter = DateFormat('HH:mm');
+      _formattedDate = _formatter.format(moment);
+    } else if (_dateDifference == 1) {
+      _formatter = DateFormat('\'Yesterday\' HH:mm');
+    } else {
+      _formatter = DateFormat('E MMM d, yyyy HH:mm');
+    }
+    _formattedDate = _formatter.format(moment);
+    print('Formatted $moment: $_formattedDate');
+    return _formattedDate;
+  }
+
+  List<Widget> myMessage(BuildContext context) {
+    return <Widget>[
+      Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            Text(
+              _format(this.now),
+              style: Theme.of(context).textTheme.bodyText2,
+            ),
+            Text(
+              this.name,
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 5.0),
+              child: Text(text),
+            ),
+          ],
+        ),
+      ),
+      Container(
+        margin: EdgeInsets.only(left: 16.0),
+        child: CircleAvatar(
+          child: Text(this.name[0]),
+        ),
+      ),
+    ];
+  }
+
+  List<Widget> otherMessage(BuildContext context) {
+    return <Widget>[
+      Container(
+        margin: EdgeInsets.only(right: 16.0),
+        child: CircleAvatar(
+          child: Image.asset('assets/aramco.png'),
+        ),
+      ),
+      Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: richResponse(context),
+          // <Widget>[
+          //   Text(
+          //     this.name,
+          //     style: TextStyle(fontWeight: FontWeight.bold),
+          //   ),
+          //   Container(
+          //     margin: EdgeInsets.only(top: 5.0),
+          //     child: Text(text),
+          //   ),
+          // ],
+        ),
+      ),
+    ];
+  }
+
+  List<Widget> _textResponse(BuildContext context) {
+    return <Widget>[
+      Text(
+        this.name,
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+      Container(
+        margin: EdgeInsets.only(top: 5.0),
+        child: Text(text),
+      ),
+    ];
+  }
+
+  /// This method renders the rich responses from Dialogflow
+  List<Widget> richResponse(BuildContext context) {
+    List<Widget> _response = [];
+    print('Rich response size: ${_response.length}');
+    _response.addAll(_textResponse(context));
+    print('Rich response size: ${_response.length}');
+
+    // Process each message in the query result.
+    for (int i = 0; i < aiResponse.getListMessage().length; ++i) {
+      // Determine the rich response type
+      // Render rich response based on its type
+      var msg = aiResponse.getListMessage()[i];
+      if (msg is df.Suggestions) {
+        _response.add(renderSuggestions(context, msg));
+        print(
+            'Added suggestion chips. Rich response size: ${_response.length}');
+      } else if (msg is df.BasicCard) {
+        _response.add(renderBasicCard(context, msg));
+        print('Added basic card. Rich response size: ${_response.length}');
+      }
+    }
+    return _response;
+  }
+
+  /// This method displays the UI of a Basic Card
+  Widget renderBasicCard(BuildContext context, df.BasicCard msg) {
+    List<Widget> buttons = [];
+    // Add basic card buttons
+    for (int i = 0; i < msg.buttons.length; ++i) {
+      buttons.add(ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.black,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+          textStyle: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        child: Text(
+          msg.buttons[i].title,
+        ),
+        onPressed: () => launch(msg.buttons[i].openUriAction.uri),
+      ));
+    }
+    // Return a container with a column containing all components
+    // of the Basic Card
+    return Container(
+      padding: EdgeInsets.all(5.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        border: Border.all(width: 0.25),
+        color: Colors.blueGrey[50],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          // Text(msg.title ?? 'Basic Card Title',
+          //   style: Theme.of(context).textTheme.headline5,
+          // ),
+          // Text(msg.subtitle ?? 'Sub Title',
+          //   style: Theme.of(context).textTheme.subtitle1,
+          // ),
+          Text(
+            msg.formattedText,
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.normal,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Image.network(msg.image.imageUri),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: buttons,
+          ),
+        ],
+      ),
+    );
+  }
+
+  /// This method displays the UI for Suggestion chips
+  Widget renderSuggestions(BuildContext context, df.Suggestions msg) {
+    List<Widget> chips = [];
+
+    for (int i = 0; i < msg.suggestions.length; ++i) {
+      chips.add(ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Theme.of(context).primaryColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+          textStyle: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        child: Text(
+          msg.suggestions[i].title,
+        ),
+        onPressed: () =>
+            this.pageState.handleSubmitted(msg.suggestions[i].title),
+      ));
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: chips,
+    );
+  }
+}
